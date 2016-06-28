5d0205cc21e15ce83deb7251170b65063105e492
fiveinfinity <jlachance1@gmail.com>
2016-06-28 13:08:18 -0500

    added addtional CSS styling

--- app/assets/javascripts/app/controllers/FavoriteController.js
+++ app/assets/javascripts/app/controllers/FavoriteController.js
@@ -1,19 +1,7 @@
 function FavoriteController(posts, user, FavoriteService) {
   var ctrl = this;
   ctrl.user = user;
-
-  ctrl.getUsersFavorites = function() {
-    ctrl.posts = posts.data;
-    return ctrl.posts.forEach(function(post) {
-      post.favorites.forEach(function(fav) {
-        if (fav.user_id !== ctrl.user.id) {
-          post.favorites.splice(fav, 1);
-        }
-      });
-    });
-  }
-
-  ctrl.getUsersFavorites();
+  ctrl.posts = FavoriteService.getUsersFavorites(posts, ctrl.user);
 }
 
 angular
--- app/assets/javascripts/app/services/FavoriteService.js
+++ app/assets/javascripts/app/services/FavoriteService.js
@@ -11,6 +11,19 @@ function FavoriteService($http, PostService) {
   this.destroyFavorite = function(id) {
     return $http.delete('/api/v1/favorites/'+id+'.json');
   }
+
+  //gets users favorites
+  this.getUsersFavorites = function(posts, user) {
+    posts = posts.data;
+    posts.forEach(function(post) {
+      post.favorites.forEach(function(fav) {
+        if (fav.user_id !== user.id) {
+          post.favorites.splice(fav, 1);
+        }
+      });
+    });
+    return posts;
+  }
 }
 
 angular
